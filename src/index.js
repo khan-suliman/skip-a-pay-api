7495fccbd3c770c10c7620cb0ea51c95c11db040
Sayed Suhail <sayedsuhail006@gmail.com>
2023-07-01 10:54:28 +0500

    Loan module/router, and CORS added.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 const express = require("express")
 require("./db/mongoose")
+const cors = require("cors")
 const adminRouter = require("./routers/admin")
 const userRouter = require("./routers/user")
+const loanRouter = require("./routers/loan")
 
 const app = express()
 const port = process.env.PORT
 
+app.use(cors())
 app.use(express.json()) // parse the incoming requests with JSON payloads and is based upon the bodyparser
 app.use(adminRouter)
 app.use(userRouter)
+app.use(loanRouter)
 
 // listen to port
 app.listen(port, () => {
