b29c8dc212443477b40d214068c171350bd8e1fb
SulimanKhan <sulimank418@gmail.com>
2023-07-01 10:40:56 +0500

    CORS added, error message added

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
-const express = require("express")
-require("./db/mongoose")
-const adminRouter = require("./routers/admin")
-const userRouter = require("./routers/user")
+const express = require("express");
+const cors = require("cors");
+require("./db/mongoose");
+const adminRouter = require("./routers/admin");
+const userRouter = require("./routers/user");
 
-const app = express()
-const port = process.env.PORT
+const app = express();
+// Apply CORS middleware
+app.use(cors());
+const port = process.env.PORT;
 
-app.use(express.json()) // parse the incoming requests with JSON payloads and is based upon the bodyparser
-app.use(adminRouter)
-app.use(userRouter)
+app.use(express.json()); // parse the incoming requests with JSON payloads and is based upon the bodyparser
+app.use(adminRouter);
+app.use(userRouter);
 
 // listen to port
 app.listen(port, () => {
-  console.log("Server is up on port " + port)
-})
+  console.log("Server is up on port " + port);
+});
--- src/routers/admin.js
+++ src/routers/admin.js
@@ -1,22 +1,22 @@
-const express = require("express")
-const Admin = require("../models/admin")
-const auth = require("../middleware/auth")
-const router = new express.Router()
+const express = require("express");
+const Admin = require("../models/admin");
+const auth = require("../middleware/auth");
+const router = new express.Router();
 
 // create admin
 router.post("/admins", async (req, res) => {
-  const admin = new Admin(req.body)
+  const admin = new Admin(req.body);
 
   try {
-    await admin.save()
+    await admin.save();
 
-    const token = await admin.generateAuthToken()
+    const token = await admin.generateAuthToken();
 
-    res.status(201).send({ admin, token })
+    res.status(201).send({ admin, token });
   } catch (e) {
-    res.status(400).send(e)
+    res.status(400).send(e);
   }
-})
+});
 
 // login admin
 router.post("/admins/login", async (req, res) => {
@@ -25,80 +25,80 @@ router.post("/admins/login", async (req, res) => {
     const admin = await Admin.findByCredentials(
       req.body.email,
       req.body.password
-    )
+    );
 
-    const token = await admin.generateAuthToken()
+    const token = await admin.generateAuthToken();
 
-    res.send({ admin, token })
+    res.send({ admin, token });
   } catch (e) {
-    res.status(400).send()
+    res.status(400).send({ error: e.message });
   }
-})
+});
 
 // logout admin
 router.post("/admins/logout", auth, async (req, res) => {
   try {
     req.admin.tokens = req.admin.tokens.filter(
       (token) => token.token !== req.token
-    )
+    );
 
-    await req.admin.save()
+    await req.admin.save();
 
-    res.send()
+    res.send();
   } catch (e) {
-    res.status(500).send()
+    res.status(500).send();
   }
-})
+});
 
 // logout admin from all devices
 router.post("/admins/logoutAll", auth, async (req, res) => {
-  console.log(req.admin)
+  console.log(req.admin);
   try {
-    req.admin.tokens = []
+    req.admin.tokens = [];
 
-    await req.admin.save()
+    await req.admin.save();
 
-    res.send()
+    res.send();
   } catch (e) {
-    res.status(500).send()
+    res.status(500).send();
   }
-})
+});
 
 // get current admin
 router.get("/admins/me", auth, async (req, res) => {
-  res.send(req.admin)
-})
+  res.send(req.admin);
+});
 
 // update the admin
 router.patch("/admins/me", auth, async (req, res) => {
-  const updates = Object.keys(req.body)
-  const allowedUpdates = ["name", "email", "password"]
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ["name", "email", "password"];
   const isValidOperation = updates.every((update) =>
     allowedUpdates.includes(update)
-  )
+  );
 
   if (!isValidOperation) {
-    return res.status(400).send({ error: "Invalid updates!" })
+    return res.status(400).send({ error: "Invalid updates!" });
   }
 
   try {
     // update each property dynamically
-    updates.forEach((update) => (req.admin[update] = req.body[update]))
-    await req.admin.save()
-    res.send(req.admin)
+    updates.forEach((update) => (req.admin[update] = req.body[update]));
+    await req.admin.save();
+    res.send(req.admin);
   } catch (e) {
-    res.status(400).send(e)
+    res.status(400).send(e);
   }
-})
+});
 
 // delete the admin
 router.delete("/admins/me", auth, async (req, res) => {
   try {
-    await req.admin.deleteOne()
-    res.status(202).send()
+    await req.admin.deleteOne();
+    res.status(202).send();
   } catch (e) {
-    res.status(500).send(e)
+    res.status(500).send(e);
   }
-})
+});
 
-module.exports = router
+module.exports = router;
