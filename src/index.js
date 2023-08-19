03db0faa1d64c0b4d384a675b3268a168328e1c4
Sayed Suhail <sayedsuhail006@gmail.com>
2023-08-19 23:44:43 +0500

    change logic for port listening!

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ const env = require("./config/env")
 
 const port = env.PORT
 
-if (port) {
-  // listen to port
-  app.listen(port, () => {
-    console.log("Server is up on port " + port)
-  })
-} else {
-  console.log("Port not availble")
-}
+app.listen(port, () => {
+  console.log("Server is up on port " + port)
+})
+
+// if (port) {
+//   // listen to port
+//   app.listen(port, () => {
+//     console.log("Server is up on port " + port)
+//   })
+// } else {
+//   console.log("Port not availble")
+// }
