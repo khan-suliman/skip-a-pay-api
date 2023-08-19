a10537f8661e5c80f5a3efbbf7d636abb8a39425
Sayed Suhail <sayedsuhail006@gmail.com>
2023-08-19 23:52:24 +0500

    reverse the changes

--- src/index.js
+++ src/index.js
@@ -3,15 +3,11 @@ const env = require("./config/env")
 
 const port = env.PORT
 
-app.listen(port, () => {
-  console.log("Server is up on port " + port)
-})
-
-// if (port) {
-//   // listen to port
-//   app.listen(port, () => {
-//     console.log("Server is up on port " + port)
-//   })
-// } else {
-//   console.log("Port not availble")
-// }
+if (port) {
+  // listen to port
+  app.listen(port, () => {
+    console.log("Server is up on port " + port)
+  })
+} else {
+  console.log("Port not availble")
+}
